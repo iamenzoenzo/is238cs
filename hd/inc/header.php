c1eef300086ae0156000cc79d6627e1158ac97b9
myname <myemail@example.com>
2019-11-06 19:58:21 +0800

    added background image to helpdesk login page

--- hd/inc/header.php
+++ hd/inc/header.php
@@ -6,4 +6,17 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<style>
+    body  {
+        background-image: url("../hd-bg-2.jpg");
+        /* Full height */
+        height: 100%;
+
+        /* Center and scale the image nicely */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>    
 <!-- jQuery -->
--- hd/login.php
+++ hd/login.php
@@ -6,9 +6,11 @@ if($users->isLoggedIn()) {
 $errorMessage = $users->login();
 include('inc/header.php');
 ?>
-<
-	title>PLEMA - Philippine Local eMergency App</title>
-<
+
+
+
+<title>PLEMA - Philippine Local eMergency App</title>
+
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
