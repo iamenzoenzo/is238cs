52d436ea0f2c8f3fbc9aa9cc3766e0a5b3169db3
myname <myemail@example.com>
2019-11-07 22:51:26 +0800

    updated ums login page

--- ums/include/header.php
+++ ums/include/header.php
@@ -2,8 +2,25 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <!-- jQuery -->
+
+<style>
+    body  {
+        background-image: url("img/hd-bg-2.jpg");
+        /* Full height */
+        height: 100%;
+
+        /* Center and scale the image nicely */
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+        background-color: #464646;
+    }
+</style>
--- ums/login.php
+++ ums/login.php
@@ -5,6 +5,9 @@ $errorMessage =  $user->login();
 include('include/header.php');
 ?>
 
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 <title>PLeMA - Philippine Local eMergency App User Management</title>
 
 <div class="container contact" style="margin-top: 10%">	
@@ -12,8 +15,8 @@ include('include/header.php');
 	<div class="row justify-content-center">
 		<div class="col-md-6">                    
 			<div class="panel panel-info" >
-				<div class="panel-heading">
-					<div class="panel-title">Log In</div>                        
+				<div class="panel-heading" style="background:#00796B;color:white;">
+					<div class="panel-title display-4">User Login</div>                        
 				</div> 
 				<div style="padding-top:30px" class="panel-body" >
 					<?php if ($errorMessage != '') { ?>
@@ -21,26 +24,30 @@ include('include/header.php');
 					<?php } ?>
 					<form id="loginform" class="form-horizontal" role="form" method="POST" action="">                                    
 						<div style="margin-bottom: 25px" class="input-group">
-							<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-							<input type="text" class="form-control" id="loginId" name="loginId"  value="<?php if(isset($_COOKIE["loginId"])) { echo $_COOKIE["loginId"]; } ?>" placeholder="email">                                        
+							<span class="input-group-addon" style = "padding-right:25px;"><i class="glyphicon glyphicon-user"></i></span>
+							<input type="text" class="form-control" id="loginId" name="loginId"  value="<?php if(isset($_COOKIE["loginId"])) { echo $_COOKIE["loginId"]; } ?>" placeholder="Email Address">                                        
 						</div>                                
 						<div style="margin-bottom: 25px" class="input-group">
-							<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-							<input type="password" class="form-control" id="loginPass" name="loginPass" value="<?php if(isset($_COOKIE["loginPass"])) { echo $_COOKIE["loginPass"]; } ?>" placeholder="password">
-						</div>            
-						<div class="input-group">
-							<div class="checkbox">
-							<label>
-								<input  type="checkbox" id="remember" name="remember" <?php if(isset($_COOKIE["loginId"])) { ?> checked <?php } ?>> Remember me
-							</label>
-							<label><a href="forget_password.php">Forget your password</a></label>	
+							<span class="input-group-addon" style = "padding-right:25px;"><i class="glyphicon glyphicon-lock"></i></span>
+							<input type="password" class="form-control" id="loginPass" name="loginPass" value="<?php if(isset($_COOKIE["loginPass"])) { echo $_COOKIE["loginPass"]; } ?>" placeholder="Password">
+						</div>
+						<div class="row">
+							<div class="input-group col-md-9" style="margin-left:25px;">
+								<div class="checkbox">
+								<label>
+									<input  type="checkbox" id="remember" name="remember" <?php if(isset($_COOKIE["loginId"])) { ?> checked <?php } ?>> Remember me
+								</label>
+								<label><a href="forget_password.php">Forget your password</a></label>	
+								</div>
 							</div>
-						</div>
-						<div style="margin-top:10px" class="form-group">                               
-							<div class="col-sm-12 controls">
-								<input type="submit" name="login" value="Login" class="btn btn-info">						  
-							</div>						
-						</div>
+							<div class="form-group col-md-3" style="padding-left:35px;">                               
+								<div class="controls">
+									<input type="submit" name="login" value="Login" class="btn btn-success btn-primary btn-lg" style="padding-right:20px; padding-left:20px;">						  
+								</div>						
+							</div>
+						</div>            
+						
+						
 						<div class="form-group">
 							<div class="col-md-12 control">
 								<div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
@@ -59,4 +66,9 @@ include('include/header.php');
 	
 	
 </div>	
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 <?php include('include/footer.php');?>
