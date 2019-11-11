43de61ec9a4dffff1fe4337ede176e0bcddea243
myname <myemail@example.com>
2019-11-11 21:45:55 +0800

    updated hd dashboard interface

--- hd/index.php
+++ hd/index.php
@@ -3,40 +3,51 @@ include 'init.php';
 if(!$users->isLoggedIn()) {
 	header("Location: login.php");	
 }
+// include('inc/header.php');
 include('inc/header.php');
 $user = $users->getUserInfo();
 ?>
-<title>webdamn.com : Demo Build Helpdesk System with PHP & MySQL</title>
+<title>PLEMA - Philippine Local eMergency App</title>
+
 <script src="js/jquery.dataTables.min.js"></script>
 <script src="js/dataTables.bootstrap.min.js"></script>		
 <link rel="stylesheet" href="css/dataTables.bootstrap.min.css" />
 <script src="js/ajax.js"></script>
 <link rel="stylesheet" href="css/style.css" />
+
+<?php //include('inc/container.php');?>
 <?php include('inc/container.php');?>
-<div class="container">	
+
+<div class="container dash-container rounded">	
 	<div class="row home-sections">
-	<h2>Example: Build Helpdesk System with PHP & MySQL</h2>	
-	<?php include('menus.php'); ?>		
-	</div> 
-	<div class="">   		
-		<p>View and manage tickets that may have responses from support team.</p>		
-		<table id="listTickets" class="table table-bordered table-striped">
-			<thead>
-				<tr>
-					<th>S/N</th>
-					<th>Ticket ID</th>
-					<th>Subject</th>
-					<th>Department</th>
-					<th>Created By</th>					
-					<th>Created</th>	
-					<th>Status</th>
-					<th></th>
-					<th></th>
-					<th></th>					
-				</tr>
-			</thead>
-		</table>
+		<?php include('menus.php'); ?>		
 	</div>
+	<div>
+		<div class="">   		
+			<p>This is where you view and manage your tickets.</p>
+			<div style="margin-right:15px; margin-left:0px; margin-bottom:0px" class="form-group">
+				<div class="controls col-md-6" style="margin-bottom:0px; padding-left:0px;">
+					<a href="#" id="createTicket" class="btn btn-info btn-primary btn-sm" style="padding-right:20px; padding-left:20px; margin-bottom:0px; margin-top:5px;">Create Ticket</a>
+				</div>
+			</div>
+			<table id="listTickets" class="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th>S/N</th>
+						<th>Ticket ID</th>
+						<th>Subject</th>
+						<th>Created By</th>					
+						<th>Created</th>
+						<th>Assignee</th>	
+						<th>Status</th>
+						<th></th>
+						<th></th>
+						<th></th>					
+					</tr>
+				</thead>
+			</table>
+		</div>
+	</div> 
 	<?php include('add_ticket_model.php'); ?>
 </div>	
 <?php include('inc/footer.php');?>
--- hd/menus.php
+++ hd/menus.php
@@ -3,13 +3,13 @@
 		<div class="navbar-header">
 			<a href="index.php" class="navbar-brand" id="index_menu">Dashboard</a>
 		</div>
-		<ul class="nav navbar-nav menus">		
-			<li><a href="#" id="createTicket">Create Ticket</a></li>			
-		</ul>
 		<ul class="nav navbar-nav navbar-right">
 			<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count"></span> 
-				<img src="//gravatar.com/avatar/<?php echo md5($user['email']); ?>?s=100" width="20px">&nbsp;<?php if(isset($_SESSION["userid"])) { echo $user['nick_name']; } ?></a>
+				<span class="label label-pill label-danger count"></span>
+					<span class="label label-pill label-danger count"></span>
+					<span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
+				</a> 
 				<ul class="dropdown-menu">
 					<li><a href="settings.php">Account</a></li>
 					<li><a href="logout.php">Logout</a></li>
