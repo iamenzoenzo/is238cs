96803376bc51472bee0abe4e5270445e690a81fd
jeffmanhulad <jeffrymanhulad@gmail.com>
2019-11-17 00:17:52 +0800

    final redirect code

--- redirect.php
+++ redirect.php
@@ -1,16 +1,31 @@
 <?php 
 include 'hd/init.php'; 
 
-/*
+
 //FINAL CODE SAVINGS SUBSCRIBERS
 if(isset($_GET["subscriber_number"])&& isset($_GET["access_token"])){
-  $subs->saveSubscriber($_GET["subscriber_number"],$_GET["access_token"]);
-}else{echo "Invalid invoke! Please subscribe thru SMS.";}
 
-$subscriberId = $subs->getSubscriberId($_GET["subscriber_number"]);
+  $subscriberId = $subs->getSubscriberIdByNumber($_GET["subscriber_number"]);
 
-echo $subs->getSubscriberNumberById($_GET["subscriber_number"]);
-*/
+  if(isset($subscriberId)){
+    $access_token = $subs->getAccessTokenById($subscriberId);
+    if($access_token!=$_GET["access_token"]){
+      //update access token
+      $subs->updateSubscriber($subscriberId,$_GET["access_token"]);
+    }else{
+      $subs->saveSubscriber($_GET["subscriber_number"],$_GET["access_token"]);
+    }
+  }
+  
+}
+else{
+  echo "Invalid invoke! Please subscribe thru SMS.";
+}
+
+
+
+
+
 
 
 
