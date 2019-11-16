bfa6827351c5adb6b80e00e2d801e7ebf48a9462
jeffmanhulad <jeffrymanhulad@gmail.com>
2019-11-17 00:18:26 +0800

    subscriber class update

--- hd/class/Subscriber.php
+++ hd/class/Subscriber.php
@@ -1,24 +1,25 @@
 <?php
 
-class Subscribers extends Database {  
+class Subscriber extends Database {  
  private $subscribersTable = 'Subscribers';
 	private $dbConnect = false;
  
 	public function __construct(){		
         $this->dbConnect = $this->dbConnect();
     } 
-
+ 
 public function saveSubscriber($Mobile_Number,$Access_Token) {
-	//final code	
+		
     $date = new DateTime();
     $date = $date->getTimestamp();
     $sqlQuery = "INSERT INTO ".$this->subscribersTable." (mobileNumber, accessToken) 
         VALUES('".$Mobile_Number."', '".$Access_Token."')";
-    return $result = mysqli_query($this->dbConnect, $sqlQuery);
-}
+    $result = mysqli_query($this->dbConnect, $sqlQuery);
+    return "Result: ".$result;
+    }
     
 public function getSubscriberIdByNumber($subscriberNumber){
-    //final code
+
     $sqlQuery = "SELECT Subscribers.idSubscribers FROM ".$this->subscribersTable." WHERE  mobileNumber='".$subscriberNumber."'";
     $result = mysqli_query($this->dbConnect, $sqlQuery);
     $row = mysqli_fetch_array($result);
@@ -28,34 +29,74 @@ public function getSubscriberIdByNumber($subscriberNumber){
     }else{return 0;}
 }
 
-public function getSubscriberNumberById($subscriberId){
+public function getAccessTokenById($subscriberId){
     //final code
+    $sqlQuery = "SELECT Subscribers.accessToken FROM ".$this->subscribersTable." WHERE idSubscribers='".$subscriberId."'";
+    $result = mysqli_query($this->dbConnect, $sqlQuery);		
+    $row = mysqli_fetch_array($result);
+    $access_token = $row['accessToken']; 
+    if(isset($access_token)){
+        return $access_token;
+    }
+    else{
+        return 0;
+    }     
+}
+
+
+public function getSubscriberNumberById($subscriberId){
+
     $sqlQuery = "SELECT Subscribers.mobileNumber FROM ".$this->subscribersTable." WHERE idSubscribers='".$subscriberId."'";
     $result = mysqli_query($this->dbConnect, $sqlQuery);		
     $row = mysqli_fetch_array($result);
     $mobileNumber = $row['mobileNumber']; 
-    if(isset($mobileNumber)){
-        return $mobileNumber;
-    }
-    else{
-        return 0;
-    } 
+    return mobileNumber;
     
 }
+
 
 public function updateSubscriber($subscriberId,$accessToken){
 
     $sqlQuery = "
-    UPDATE ".$database.".".$this->subscribersTable." 
-    SET 'accessToken' = '".$accessToken."'
-    WHERE `id` = ".$subscriberId.";
+    UPDATE ".$database."".$this->subscribersTable." 
+    SET accessToken = '".$accessToken."'
+    WHERE idSubscribers = ".$subscriberId.";
     ";
-    $result = mysqli_query($this->dbConnect, $sqlQuery);
-    return $result;
+    mysqli_query($this->dbConnect, $sqlQuery);
+    return "success";
     
 }
 
-//------------------------------------------------------------------------------
+
+
+
+/*
+
+public function getSubscriberInfoByMobileNumber($MobileNumber){
+    
+    if(!empty($MobileNumber)) {
+        $sqlQuery = "SELECT * FROM ".$this->subscribersTable." WHERE mobileNumber ='".$MobileNumber."'";
+        $result = mysqli_query($this->dbConnect, $sqlQuery);		
+        $userDetails = mysqli_fetch_assoc($result);        
+        return $userDetails;
+    }
+}
+
+public function getSubscriberIdByNumber($subscriberNumber){
+
+    $sqlQuery = "SELECT Subscribers.idSubscribers FROM ".$this->subscribersTable." WHERE  mobileNumber='".$subscriberNumber."'";
+    $result = mysqli_query($this->dbConnect, $sqlQuery);		
+    $userId = mysqli_fetch_assoc($result);
+    if($userId!=0){
+        return $userId['idSubscribers'];
+    }else{return 0;}
+    
+    
+}
+
+
+    
+
 public function getSubscriberInfoFromDb(){
     
     if(!empty($_SESSION["userid"])) {
@@ -79,7 +120,16 @@ public function getAllSubscribers(){
 
 
 
-  
+  public function updateSubscriberInfo($subscriberId,$subscriberNumber,$accessToken){
+
+      $sqlQuery = "
+      UPDATE ".$database.".".$this->subscriberTable." 
+      SET `subscriber_number` = '".$subscriberNumber."', `subscriber_access_token` = '".$accessToken."'
+      WHERE `id` = ".$subscriberId.";
+      ";
+      $result = mysqli_query($this->dbConnect, $sqlQuery);		
+      
+  }
 
   public function removeSubscriber(){
 
@@ -90,6 +140,6 @@ public function getAllSubscribers(){
       
   }
 
- 
+ */
 
 }
