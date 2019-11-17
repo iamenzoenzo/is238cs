72bfab510fb9e29c8212cef7f36ac798f7b0c95d
jeffmanhulad <jeffrymanhulad@gmail.com>
2019-11-17 23:39:38 +0800

    saving to ticket_replies

--- notify.php
+++ notify.php
@@ -3,6 +3,8 @@ include 'hd/init.php';
 
 $KEYWORDS = array("PLEMA-HELP","PLEMA-REPLY");
 //FOR TESTING: Comment in final code
+
+/*
 $json_data = '{
     "inboundSMSMessageList": {
       "inboundSMSMessage": [
@@ -10,7 +12,7 @@ $json_data = '{
           "dateTime": "Sun Nov 17 2019 08:21:05 GMT+0000 (UTC)",
           "destinationAddress": "tel:21588207",
           "messageId": "5dd102f145526e546defac59",
-          "message": "PLEMA-REPLY 6CE8F This is a reply to my last message.",
+          "message": "PLEMA-REPLY 6CE8F This is a reply to my last message. Another test",
           "resourceURL": null,
           "senderAddress": "tel:+639271280446"
         }
@@ -20,10 +22,11 @@ $json_data = '{
       "totalNumberOfPendingMessages": 0
     }
   }';
+*/
 
 //FINAL CODE: Uncomment in final code
 //get POST data from globelabs API via file_get_contents method
-//$json_data = file_get_contents('php://input');
+$json_data = file_get_contents('php://input');
 //decode json data into array
 $jason_arr = json_decode($json_data,true);
 
@@ -49,53 +52,60 @@ if($numberOfMessagesInThisBatch>1){
 
 //-----------TESTING
 
-//Save the message
-//$messageDbId = $inbound->saveMessages($dateTime,$destinationAddress,$messId,$message,$resourceURL,$senderAddress,$numberOfMessagesInThisBatch,$totalNumberOfPendingMessages,$multipartRefId,$multipartSeqNum,0);
+if(count($jason_arr)!=0){    
+    //Save the message
+    $messageDbId = $inbound->saveMessages($dateTime,$destinationAddress,$messId,$message,$resourceURL,$senderAddress,$numberOfMessagesInThisBatch,$totalNumberOfPendingMessages,$multipartRefId,$multipartSeqNum,0);
 
-//Check if multipart message, if YES, proceed to STEP 3
-if($numberOfMessagesInThisBatch==1){
-    //check if this message is a reply
-    if(isReply($message,$KEYWORDS)){
-        //get ticket reference
-        $ticketRef = getReplyTicketReference($message);
-        $validReference = $inbound->validTicketReference($MobileNo,$ticketRef);
-        if($validReference){
-            //save to replies table.
-            $mes = getReplyMessage($message);
-            $date = date_format(date_create($dateTime),"Y/m/d H:i:s");
-            $inbound->saveToReplies($ticketRef,$mes,$date);
+    //Check if multipart message, if YES, proceed to STEP 3
+    if($numberOfMessagesInThisBatch==1){
+        //check if this message is a reply
+        if(isReply($message,$KEYWORDS)){
+            //get ticket reference
+            $ticketRef = getReplyTicketReference($message);
+            $validReference = $inbound->validTicketReference($MobileNo,$ticketRef);
+            if($validReference){
+                //save to replies table.
+                $mes = getReplyMessage($message);
+                $date = date_format(date_create($dateTime),"Y/m/d H:i:s");
+                $ticketId = $inbound->getTicketIdByByReference($ticketRef,$MobileNo);
+                $inbound->saveToReplies($ticketId,$mes,$date);
+            }else{
+                //Reply to user and remind to use valid ticket reference.
+                echo "invalid ticket reference";
+            }
+            
         }else{
-            //Reply to user and remind to use valid ticket reference.
-            echo "invalid ticket reference";
+            //not a reply. Save to tickets table
+            $ticketId = $inbound->saveToTickets($MobileNo,$message,'Open');
+            if($ticketId!=0){
+                //Delete message by id
+                $inbound->deleteMessagesByMessageId($messId);
+            }    
+        }
+    }else{
+        //check if all messages were received, else do nothing
+        $allMessagesReceived = $inbound->checkIfAllMessagesReceived($multipartRefId,$numberOfMessagesInThisBatch);
+        if($allMessagesReceived=1){
+            //get all messages and concat it.
+            $message="";
+            $messages = $inbound->getMessagesByMultipartReferenceId($multipartRefId);
+            foreach($messages as $mess){
+                $message=$message.$mess['message'];
+            }
+            //get userId by mobile number
+            //Save to tickets table
+            //delete the messages by multipart ref Id
+            //$inbound->deleteMessagesByMultipartRefId($multipartRefId);
+        }else{
+            echo "still have pending messages";
         }
         
-    }else{
-        //not a reply. Save to tickets table
-        $ticketId = $inbound->saveToTickets($MobileNo,$message,'Open');
-        if($ticketId!=0){
-            //Delete message by id
-            $inbound->deleteMessagesByMessageId($messId);
-        }    
-    }
+    } 
 }else{
-    //check if all messages were received, else do nothing
-    $allMessagesReceived = $inbound->checkIfAllMessagesReceived($multipartRefId,$numberOfMessagesInThisBatch);
-    if($allMessagesReceived=1){
-        //get all messages and concat it.
-        $message="";
-        $messages = $inbound->getMessagesByMultipartReferenceId($multipartRefId);
-        foreach($messages as $mess){
-            $message=$message.$mess['message'];
-        }
-        //get userId by mobile number
-        //Save to tickets table
-        //delete the messages by multipart ref Id
-        //$inbound->deleteMessagesByMultipartRefId($multipartRefId);
-    }else{
-        echo "still have pending messages";
-    }
-    
-} 
+    echo "no message";
+}
+
+
 
 //FUNCTIONS
 function isReply($textMessage,$keywords){
