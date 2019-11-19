8c3ee75623597a6d453195f2c8177310536594cd
Benson Cerbas <benson.cerbas@upou.edu.ph>
2019-11-19 22:38:06 +0800

    config and init

---
+++ helpdesk/config.php
@@ -0,0 +1,6 @@
+<?php
+$host = "teamlaban.chioee9qrhwf.us-east-1.rds.amazonaws.com";
+$username = "teamlaban";
+$password = "teamlaban";
+$database = "teamlaban";
+?>
---
+++ helpdesk/init.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include 'config.php';
+define('HOST', $host);
+define('USER', $username);
+define('PASSWORD', $password);
+define('DATABASE', $database);
+require 'class/Database.php';
+require 'class/Users.php';
+require 'class/Time.php';
+require 'class/Tickets.php';
+require 'class/Subscriber.php';
+require 'class/InboundSms.php';
+require 'class/OutboundSms.php';
+$database = new Database;
+$users = new Users;
+$time = new Time;
+$tickets = new Tickets;
+$subs = new Subscriber;
+$inbound = new InboundSms;
+$outbound = new OutboundSms;
+?>
