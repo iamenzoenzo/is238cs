3b515e8ae7c49cfaca46f19d6044bfbe558da693
jeffmanhulad <jeffrymanhulad@gmail.com>
2019-11-21 00:02:28 +0800

    transferred other functions

--- helpdesk/class/InboundSms.php
+++ helpdesk/class/InboundSms.php
@@ -11,15 +11,16 @@ class InboundSms extends Database {
 	}
 
 public function saveMessages($dateTime,$destinationAddress,$messageID,$message,$resourceURL,$senderAddress,$numberOfMessageInThisBatch,$totalNumberOfPendingMsg,$multipartRefId,$multipartSeqNum,$isDeleted) {
-	//final code
-	$sqlQuery = "INSERT INTO ".$this->messagesTable." (dateTime,destinationAddress,messageID,message,resourceURL,senderAddress,numberOfMessageInThisBatch,totalNumberOfPendingMsg,multipartRefId,multipartSeqNum,isDeleted) VALUES('".$dateTime."', '".$destinationAddress."', '".$messageID."', '".$message."', '".$resourceURL."', '".$senderAddress."', ".$numberOfMessageInThisBatch.", ".$totalNumberOfPendingMsg.", '".$multipartRefId."', ".$multipartSeqNum.", ".$isDeleted.")";
+	
+	$sqlQuery = "INSERT INTO ".$this->messagesTable." (dateTime,destinationAddress,messageID,message,resourceURL,senderAddress,numberOfMessageInThisBatch,totalNumberOfPendingMsg,multipartRefId,multipartSeqNum,isDeleted) VALUES('".$dateTime."', '".$destinationAddress."', '".$messageID."', '".$message."', '".$resourceURL."', '".$senderAddress."', ".$numberOfMessageInThisBatch.", ".$totalNumberOfPendingMsg.", '".$multipartRefId."', '".$multipartSeqNum."', ".$isDeleted.")";
 	$result = mysqli_query($this->dbConnect, $sqlQuery);
 	return $result;
 }
 
 public function getMessagesByMultipartReferenceId($multipartRefId){
 	$sqlQuery = "SELECT * FROM ".$this->messagesTable.
-	" WHERE  multipartRefId='".$multipartRefId."'  ORDER BY multipartSeqNum ASC";
+	" WHERE  multipartRefId='".$multipartRefId.
+	"'  ORDER BY multipartSeqNum ASC";
 	$result = mysqli_query($this->dbConnect, $sqlQuery);
 	$data= array();
 		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
@@ -61,8 +62,7 @@ public function deleteMessagesByMultipartRefId($multipartRefId){
 }
 
 public function deleteMessagesByDbId($messageDbId){
-    $sqlQuery = "
-    UPDATE ".$this->messagesTable."
+    $sqlQuery = "UPDATE ".$this->messagesTable."
     SET isDeleted = 1 WHERE id = ".$messageDbId.";";
     mysqli_query($this->dbConnect, $sqlQuery);
     return "success";
@@ -87,14 +87,14 @@ public function saveToTickets($MobileNumber,$message,$Status,$ticketRef) {
 }
 
 public function saveToReplies($ticketId,$message,$datereplied) {
-	$sqlQuery = "INSERT INTO ".$this->repliesTable." (ticket_id,Reply,Date)
+	$sqlQuery = "INSERT INTO ".$this->repliesTable." (ticket_id,Reply,created)
 	VALUES(".$ticketId.",'".$message."', '".$datereplied."');";
 	$result = mysqli_query($this->dbConnect, $sqlQuery);
 	return $result;
 }
 
 public function getTicketIdByByReference($TicketReference,$MobileNumber){
-    //final code
+    
     $sqlQuery = "SELECT Tickets.idTickets FROM ".$this->ticketsTable.
     " WHERE TicketReference='".$TicketReference."' AND MobileNumber='".$MobileNumber."';";
     $result = mysqli_query($this->dbConnect, $sqlQuery);
@@ -107,6 +107,27 @@ public function getTicketIdByByReference($TicketReference,$MobileNumber){
         return 0;
 	}
 
+}
+
+public function isReply($textMessage,$keywords_list){
+
+	$key = explode(' ', $textMessage,2);
+	
+    if(in_array(strtoupper($key[0]),$keywords_list)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+public function getReplyTicketReference($textMessage){
+    $key = explode(' ', $textMessage,3);
+    return strtoupper($key[1]);
+}
+
+public function getReplyMessage($textMessage){
+    $key = explode(' ', $textMessage,3);
+    return $key[2];
 }
 
 }
