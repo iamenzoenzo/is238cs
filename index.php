a3861bd084e9a76e5ccf3e89b4f80fb06d0fa3a7
Benson Cerbas <benson.cerbas@upou.edu.ph>
2019-11-24 14:49:22 +0800

    new frontend

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html  >
 <head>
-  <!-- Site made with Mobirise Website Builder v4.11.2, https://mobirise.com -->
+  <!-- Site made with Mobirise Website Builder v4.11.5, https://mobirise.com -->
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="generator" content="Mobirise v4.11.2, mobirise.com">
+  <meta name="generator" content="Mobirise v4.11.5, mobirise.com">
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-  <link rel="shortcut icon" href="assets/images/plemalogo3-106x99.png" type="image/x-icon">
+  <link rel="shortcut icon" href="assets/images/plema-122x122.png" type="image/x-icon">
   <meta name="description" content="">
   
   <title>Home</title>
-  <link rel="stylesheet" href="assets/web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
   <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
-  <link rel="stylesheet" href="assets/tether/tether.min.css">
   <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
   <link rel="stylesheet" href="assets/dropdown/css/style.css">
   <link rel="stylesheet" href="assets/theme/css/style.css">
   <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
@@ -25,7 +24,7 @@
   
 </head>
 <body>
-  <section class="menu cid-rH1DDvJgSr" once="menu" id="menu1-6">
+  <section class="menu cid-rII4hVtsjv" once="menu" id="menu1-a">
 
     
 
@@ -41,12 +40,12 @@
         <div class="menu-logo">
             <div class="navbar-brand">
                 <span class="navbar-logo">
-                    <a href="https://plema-dev.digital.com">
-                         <img src="assets/images/plemalogo3-106x99.png" alt="Mobirise" title="" style="height: 3.8rem;">
+                    <a href="https://www.plema.digital">
+                         <img src="assets/images/plema-122x122.png" alt="PLEMA Digital" title="PLEMA Digital" style="height: 3.8rem;">
                     </a>
                 </span>
                 <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="https://mobirise.co">
-                        PLEMA</a></span>
+                        PLEMA Digital</a></span>
             </div>
         </div>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -56,32 +55,46 @@
     </nav>
 </section>
 
-<section class="engine"><a href="https://mobirise.info/z">best website templates</a></section><section class="header10 cid-rGUXfdpi7G mbr-fullscreen mbr-parallax-background" id="header10-0">
+<section class="engine"><a href="https://mobirise.info/s">bootstrap themes</a></section><section class="header8 cid-rIIK2ZOBXa" data-bg-video="https://www.youtube.com/watch?v=36YgDDJ7XSc" id="header8-c">
 
     
 
-    
-
-    <div class="container">
-        <div class="media-container-column mbr-white p-5 align-left col-lg-8 col-md-10">
-            <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">
-                PLEMA</h1>
-            
-            <p class="mbr-text pb-3 mbr-fonts-style display-5">
-               Welcome to Philippine Local EMergency App</p>
-            <div class="mbr-section-btn"><a class="btn btn-md btn-primary display-7" href="https://plema-dev.digital/hd/index.php"><span class="mbrib-help mbr-iconfont mbr-iconfont-btn"></span>Help Desk</a>
-                <a class="btn btn-md btn-primary display-7" href="https://plema-dev.digital/ums/index.php"><span class="mbrib-users mbr-iconfont mbr-iconfont-btn"></span>User Management</a></div>
-        </div>
+    <div class="mbr-overlay" style="opacity: 0.4; background-color: rgb(85, 180, 212);">
     </div>
 
-    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
-        <a href="#next">
-            <i class="mbri-down mbr-iconfont"></i>
-        </a>
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title align-center py-2 mbr-bold mbr-fonts-style display-1">
+                        PLEMA Digital</h1>
+                <p class="mbr-text align-center py-2 mbr-fonts-style display-5"><strong>
+                        Philippine Local Emergency App</strong><br>Providing emergency services communication to every Juan and Juana.</p>
+                <div class="mbr-media show-modal align-center py-2" data-modal=".modalWindow">
+                         <span class="mbr-iconfont mbri-play" style="color: rgb(20, 157, 204); fill: rgb(20, 157, 204);"></span>
+                </div>
+                
+                <div class="mbr-section-btn text-center"><a class="btn btn-md btn-primary display-4" href="https://www.plema.digital/users/login.php">Login</a></div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="modalWindow" style="display: none;">
+            <div class="modalWindow-container">
+                <div class="modalWindow-video-container">
+                    <div class="modalWindow-video">
+                        <iframe width="100%" height="100%" frameborder="0" allowfullscreen="1" data-src="https://www.youtube.com/watch?v=36YgDDJ7XSc"></iframe>
+                    </div>
+                    <a class="close" role="button" data-dismiss="modal">
+                        <span aria-hidden="true" class="mbri-close mbr-iconfont closeModal"></span>
+                        <span class="sr-only">Close</span>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
-<section class="team1 cid-rGVt0OLDoj" id="team1-1">
+<section class="team1 cid-rII1TZvdIo" id="team1-6">
     
     
     
@@ -89,42 +102,40 @@
         <h2 class="pb-3 mbr-fonts-style mbr-section-title display-2">
             OUR AWESOME TEAM
         </h2>
-        <h3 class="pb-5 mbr-section-subtitle mbr-fonts-style mbr-light display-5">
-            IS238 TeamLaban's awesome team.</h3>
+        <h3 class="pb-5 mbr-section-subtitle mbr-fonts-style mbr-light display-5">IS 238 TeamLaban Group</h3>
         <div class="row media-row">
             
         <div class="team-item col-lg-3 col-md-6">
                 <div class="item-image">
-                    <img src="assets/images/mbr-510x340.jpg" alt="" title="">
+                    <img src="assets/images/face3.jpg" alt="" title="">
                 </div>
                 <div class="item-caption py-3">
                     <div class="item-name px-2">
                         <p class="mbr-fonts-style display-5">
-                           McJoben</p>
+                           Jeff</p>
                     </div>
                     
                     
                 </div>
             </div><div class="team-item col-lg-3 col-md-6">
                 <div class="item-image">
-                    <img src="assets/images/mbr-1-510x340.jpg" alt="" title="">
+                    <img src="assets/images/face2.jpg" alt="" title="">
                 </div>
                 <div class="item-caption py-3">
                     <div class="item-name px-2">
-                        <p class="mbr-fonts-style display-5">
-                           Camille</p>
+                        <p class="mbr-fonts-style display-5"></p><p>Karen</p><p></p>
                     </div>
                     
                     
                 </div>
             </div><div class="team-item col-lg-3 col-md-6">
                 <div class="item-image">
-                    <img src="assets/images/mbr-510x340.jpg" alt="" title="">
+                    <img src="assets/images/face3.jpg" alt="" title="">
                 </div>
                 <div class="item-caption py-3">
                     <div class="item-name px-2">
                         <p class="mbr-fonts-style display-5">
-                           Jeffry</p>
+                           Joben</p>
                     </div>
                     
                     
@@ -133,7 +144,7 @@
     </div>
 </section>
 
-<section class="team1 cid-rGVtw0NHsi" id="team1-2">
+<section class="team1 cid-rII1UqP0tG" id="team1-7">
     
     
     
@@ -144,7 +155,7 @@
             
         <div class="team-item col-lg-3 col-md-6">
                 <div class="item-image">
-                    <img src="assets/images/mbr-510x340.jpg" alt="" title="">
+                    <img src="assets/images/face3.jpg" alt="" title="">
                 </div>
                 <div class="item-caption py-3">
                     <div class="item-name px-2">
@@ -156,12 +167,12 @@
                 </div>
             </div><div class="team-item col-lg-3 col-md-6">
                 <div class="item-image">
-                    <img src="assets/images/mbr-1-510x340.jpg" alt="" title="">
+                    <img src="assets/images/face2.jpg" alt="" title="">
                 </div>
                 <div class="item-caption py-3">
                     <div class="item-name px-2">
                         <p class="mbr-fonts-style display-5">
-                           Karen</p>
+                           Camille</p>
                     </div>
                     
                     
@@ -170,7 +181,7 @@
     </div>
 </section>
 
-<section once="footers" class="cid-rGVvQXwkCL" id="footer6-3">
+<section once="footers" class="cid-rIIJdWXyAP" id="footer6-b">
 
     
 
@@ -180,7 +191,7 @@
         <div class="media-container-row align-center mbr-white">
             <div class="col-12">
                 <p class="mbr-text mb-0 mbr-fonts-style display-7">
-                    © Copyright 2019 TeamLaban- All Rights Reserved
+                    © Copyright 2019 PLEMA Digital - All Rights Reserved
                 </p>
             </div>
         </div>
@@ -193,10 +204,12 @@
   <script src="assets/bootstrap/js/bootstrap.min.js"></script>
   <script src="assets/smoothscroll/smooth-scroll.js"></script>
   <script src="assets/tether/tether.min.js"></script>
-  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
-  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/ytplayer/jquery.mb.ytplayer.min.js"></script>
+  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
   <script src="assets/dropdown/js/nav-dropdown.js"></script>
   <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/playervimeo/vimeo_player.js"></script>
   <script src="assets/theme/js/script.js"></script>
   
   
