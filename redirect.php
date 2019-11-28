5a6bd84fe8ee4816395ce15e774a70f74d62b2b4
jeffmanhulad <jeffrymanhulad@gmail.com>
2019-11-28 22:45:16 +0800

    final code with auto reply after subscribing

--- redirect.php
+++ redirect.php
@@ -2,6 +2,9 @@
 include 'helpdesk/init.php';
 date_default_timezone_set('Asia/Manila');
 
+$autoReplyMessageNewSubscriber="Welcome to PLEMA! Your trusted emergency app! Thank you for subscribing. Best regards, #TeamLaban.";
+$autoReplyMessageReturningSubscriber="Welcome back to PLEMA! Thank you for trusting us. Best regards, #TeamLaban.";
+
 //check if subscriber_number and access_token has value
 if(isset($_GET["subscriber_number"])&& isset($_GET["access_token"])){
   
@@ -16,10 +19,14 @@ if(isset($_GET["subscriber_number"])&& isset($_GET["access_token"])){
       
       //update access_token
       $subs->updateSubscriber($subscriberId,$_GET["access_token"]);
+      $outbound->sendSms($api_short_code, $_GET["access_token"],$_GET["subscriber_number"],$autoReplyMessageReturningSubscriber);
+      
     }
   }else{
     //not yet subscribed
     $subs->saveSubscriber($_GET["subscriber_number"],$_GET["access_token"]);
+    $outbound->sendSms($api_short_code, $_GET["access_token"],$_GET["subscriber_number"],$autoReplyMessageNewSubscriber);
+    
   }
 }
 else{
