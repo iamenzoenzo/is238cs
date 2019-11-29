b7d7d37c71dc4aad32b849d259577f60a67d9053
jeffmanhulad <jeffrymanhulad@gmail.com>
2019-11-30 00:39:21 +0800

    testing

--- test.php
+++ test.php
@@ -49,18 +49,8 @@ if($numberOfMessagesInThisBatch>1){
     $multipartSeqNum = '';
 }
 
-$access_token = $subs->getAccessTokenByMobileNumber($MobileNo);
-
-$subs->updateSubscriber($MobileNo,$access_token);
-
-echo $phonenumber->savePhoneNumber("09268406887","MNL","Metro Manila","PNP","Jeffry Manhulad");
-$pns = $phonenumber->getPhoneNumberByAgencyCode("MNL","PNP");
-$message="";
-foreach($pns as $pn){
-    $message=$message.$pn['phone_number']."; ";
-}
-echo $message;
-
+//echo $outbound->assignAgent($MobileNumber,$AgentName);
+$outbound->returnExpiredTickets();
 /*
 
 if(isset($messId)){
