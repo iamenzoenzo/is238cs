1a11e99234b17ae5c697125dc26f5a9e2cf7bc39
jeffmanhulad <jeffrymanhulad@gmail.com>
2019-11-29 08:02:09 +0800

    PhoneNumbers class

--- helpdesk/init.php
+++ helpdesk/init.php
@@ -12,6 +12,7 @@ require 'class/Tickets.php';
 require 'class/Subscriber.php';
 require 'class/InboundSms.php';
 require 'class/OutboundSms.php';
+require 'class/PhoneNumbers.php';
 $database = new Database;
 $users = new Users;
 $time = new Time;
@@ -19,4 +20,5 @@ $tickets = new Tickets;
 $subs = new Subscriber;
 $inbound = new InboundSms;
 $outbound = new OutboundSms;
+$phonenumber = new PhoneNumbers;
 ?>
